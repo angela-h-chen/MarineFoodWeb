e01c50560e877585448349759ce48941c75a02f3
TomasMarina <tomasimarina@gmail.com>
2022-06-28 18:22:50 -0300

    Still loading food webs

--- R/0_DataLoad.R
+++ R/0_DataLoad.R
@@ -8,6 +8,7 @@
 # Load packages ----
 library(data.table)
 library(tidyverse)
+library(igraph)
 
 
 # Load data ----
@@ -25,11 +26,32 @@ mynetworks <- within(myfiles, rm(Metadata))  # remove 'Metadata' item
 
 # Convert to igraph objects ----
 # From adjacency matrix
+adj_l <- within(mynetworks, rm("Beagle Channel", "Gulf Alaska", "Potter Cove",
+                              "Sanak intertidal", "Sanak nearshore", "Weddell Sea"))
+adj_to_g <- function(x){
+  to_m <- lapply(x, as.matrix)
+  col_dis <- lapply(to_m, function(z) z[, -1])
+  g_adj <- lapply(col_dis, graph_from_adjacency_matrix)
+}
+g_adj <- adj_to_g(adj_l)
+
+m_b <- adj_l[["Baltic Sea"]]
+m_b <- as.matrix(m_b)[,-1]
+names <- colnames(m_b)
+m_t <- t(m_b)
+colnames(m_t) <- names
+
+g_b <- graph_from_adjacency_matrix(m_t)
 
 
 # From edge list
-
-
+edge_l <- mynetworks[c("Beagle Channel", "Gulf Alaska", "Potter Cove",
+                        "Sanak intertidal", "Sanak nearshore", "Weddell Sea")]
+edge_to_g <- function(x){
+  to_m <- lapply(x, as.matrix)
+  to_g <- lapply(to_m, graph_from_edgelist)
+  }
+g_edge <- edge_to_g(edge_l)
 
 
 

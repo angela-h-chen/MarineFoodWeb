8cefc7efba8ac7614d6b003d57a11dc9fc0516f7
TomasMarina <tomasimarina@gmail.com>
2022-06-29 10:00:12 -0300

    Matrix/Edge list to g objects

--- R/0_DataLoad.R
+++ R/0_DataLoad.R
@@ -25,9 +25,22 @@ mynetworks <- within(myfiles, rm(Metadata))  # remove 'Metadata' item
 
 
 # Convert to igraph objects ----
-# From adjacency matrix
+
+## From adjacency matrix ----
+
 adj_l <- within(mynetworks, rm("Beagle Channel", "Gulf Alaska", "Potter Cove",
-                              "Sanak intertidal", "Sanak nearshore", "Weddell Sea"))
+                              "Sanak intertidal", "Sanak nearshore", "Weddell Sea"))  # discard networks as edge lists
+# Transpose matrix for "Baltic Sea" & "Southern Brazil"
+m_sb <- adj_l[["Southern Brazil"]]  # m_b "Baltic Sea"
+m_sb <- as.matrix(m_sb)[,-1]  # m_b
+names <- colnames(m_sb)  # m_b
+m_t_sb <- t(m_sb)  # m_t_b m_b
+colnames(m_t_sb) <- names  # m_t_b
+# Convert matrix to igraph
+g_b <- graph_from_adjacency_matrix(m_t_b, mode = "directed")  # Baltic Sea
+g_sb <- graph_from_adjacency_matrix(m_t_sb, mode = "directed")  # Southern Brazil
+
+# Function to convert adj matrix to g objects
 adj_to_g <- function(x){
   to_m <- lapply(x, as.matrix)
   col_dis <- lapply(to_m, function(z) z[, -1])
@@ -35,16 +48,13 @@ adj_to_g <- function(x){
 }
 g_adj <- adj_to_g(adj_l)
 
-m_b <- adj_l[["Baltic Sea"]]
-m_b <- as.matrix(m_b)[,-1]
-names <- colnames(m_b)
-m_t <- t(m_b)
-colnames(m_t) <- names
-
-g_b <- graph_from_adjacency_matrix(m_t)
+# Replace g objects for "Baltic Sea" & "Southern Brazil"
+g_adj[["Baltic Sea"]] <- g_b
+g_adj[["Southern Brazil"]] <- g_sb
 
 
-# From edge list
+## From edge list ----
+
 edge_l <- mynetworks[c("Beagle Channel", "Gulf Alaska", "Potter Cove",
                         "Sanak intertidal", "Sanak nearshore", "Weddell Sea")]
 edge_to_g <- function(x){
@@ -54,6 +64,14 @@ edge_to_g <- function(x){
 g_edge <- edge_to_g(edge_l)
 
 
+# Append g lists ----
 
+g_all <- append(g_adj, g_edge)
+
+
+# Save data ----
+
+save(g_adj, g_edge, g_all,
+     file = "Data/all_igraph.rda")
 
 
